b6619ca858f6fff216c11520d1507d0b21ddfaf0
HaiderAli228 <flutter2830@gmail.com>
2025-02-02 00:24:44 +0500

    adding carosual slider

--- lib/image_crousal.dart
+++ lib/image_crousal.dart
@@ -14,6 +14,8 @@ class _ImageCarouselState extends State<ImageCarousel> {
     'assets/images/1.jpg',
     'assets/images/2.jpg',
     'assets/images/3.jpg',
+    'assets/images/4.jpg',
+    'assets/images/5.jpg',
   ];
 
   @override
